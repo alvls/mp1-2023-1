162a10a1b22b6cd5ea9444e73c24fa619e698ae2
Roman Nikolaev <roma.niko393@gmail.com>
2023-02-25 21:48:34 +0300

    Changed code style

--- nikolaev_ra/task1/length_converter.h
+++ nikolaev_ra/task1/length_converter.h
@@ -1,6 +1,7 @@
 #include <iostream>
 
-class LengthConverter {
+class LengthConverter
+{
 private:
     double length_{};
 
@@ -13,37 +14,45 @@ public:
 public:
     LengthConverter() = default;
 
-    explicit LengthConverter(double length_value) {
+    explicit LengthConverter(double length_value)
+    {
         (length_value >= 0) ? length_ = length_value : length_ = 0;
     }
 
-    [[nodiscard]] double get_length() const {
+    [[nodiscard]] double get_length() const
+    {
         return length_;
     }
 
-    void set_length(double length_value) {
+    void set_length(double length_value)
+    {
         (length_value >= 0) ? length_ = length_value : length_ = 0;
     }
 
-    [[nodiscard]] double GetLengthInFeet() const {
+    [[nodiscard]] double get_length_in_feet() const
+    {
         return length_ * foot_factor;
     }
 
-    [[nodiscard]] double GetLengthInYards() const {
+    [[nodiscard]] double get_length_in_yards() const
+    {
         return length_ * yard_factor;
     }
 
-    [[nodiscard]] double GetLengthInArshins() const {
+    [[nodiscard]] double get_length_in_arshins() const
+    {
         return length_ * arshin_factor;
     }
 
-    [[nodiscard]] double GetLengthInFathoms() const {
+    [[nodiscard]] double get_length_in_fathoms() const
+    {
         return length_ * fathom_factor;
     }
 
-    void PrintInformation() const {
-        std::cout << "LengthConverter{meters: " << length_ << ", feet: " << GetLengthInFeet() << ", yards: "
-                  << GetLengthInYards() << ", arshins: " << GetLengthInArshins() << ", fathoms: "
-                  << GetLengthInFathoms() << "}" << std::endl;
+    void print_information() const
+    {
+        std::cout << "LengthConverter{meters: " << length_ << ", feet: " << get_length_in_feet() << ", yards: " 
+                  << get_length_in_yards() << ", arshins: " << get_length_in_arshins() << ", fathoms: " 
+                  << get_length_in_fathoms() << "}" << std::endl;
     }
 };
--- nikolaev_ra/task1/source.cpp
+++ nikolaev_ra/task1/source.cpp
@@ -1,14 +1,15 @@
 #include "length_converter.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     std::cout << "Creating an object of class LengthConverter" << std::endl;
     LengthConverter lc{};
     std::cout << "Current length in meters: " << lc.get_length() << std::endl;  // 0 by default
 
     std::cout << "\nSetting value: 123.5 meters" << std::endl;
     lc.set_length(123.5);
-    std::cout << "Current length in feet: " << lc.GetLengthInFeet() << std::endl;
-    std::cout << "Current length in arshins: " << lc.GetLengthInArshins() << std::endl;
+    std::cout << "Current length in feet: " << lc.get_length_in_feet() << std::endl;
+    std::cout << "Current length in arshins: " << lc.get_length_in_arshins() << std::endl;
 
     // Now let's give you the option to enter a length value
     int length_value;
@@ -16,9 +17,9 @@ int main(int argc, char* argv[]) {
     std::cin >> length_value;
 
     lc.set_length(length_value);
-    std::cout << "Current length in yards: " << lc.GetLengthInYards() << std::endl;
-    std::cout << "Current length in fathoms: " << lc.GetLengthInFathoms() << std::endl;
-    lc.PrintInformation();
+    std::cout << "Current length in yards: " << lc.get_length_in_yards() << std::endl;
+    std::cout << "Current length in fathoms: " << lc.get_length_in_yards() << std::endl;
+    lc.print_information();
 
     system("pause");
     return EXIT_SUCCESS;
