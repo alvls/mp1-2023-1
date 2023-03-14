43de826ae237921f1f9df5a85f93773149b00642
kalyakinaa <nastyakalyakinann@gmail.com>
2023-03-14 18:49:09 +0300

    Corrections in Task 1

--- kalyakina_ad/task1/Source.cpp
+++ kalyakina_ad/task1/Source.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <conio.h>
 #include <string>
-#include <vector>
 
 #define KEY_UP 72
 #define KEY_DOWN 80
@@ -46,187 +45,85 @@ int choise_in_menu(std::string list_for_choise[], int max_number_of_choise)
 
 class Weight
 {
-	double meaning, conversed_meaning;
-	std::string system_of_measures[systems_of_measure_count];
-	std::string RSM_names[RSM_PhSM_count];
-	std::string BrSM_names[BrSM_count];
-	std::string PhSM_country[PhSM_country_count];
-	std::string PhSM_names[RSM_PhSM_count];
+	double meaning;
 public:
 	Weight()
 	{
 		meaning = 0.0;
-		conversed_meaning = 0.0;
-		system_of_measures[0] = { "Выберите систему мер веса:" }; system_of_measures[1] = { "Русская система мер" }; system_of_measures[2] = { "Британская система мер" }; system_of_measures[3] = { "Аптекарская система мер" }; system_of_measures[4] = { "Выход" };
-		RSM_names[0] = { "Выберите единицу измерений:" }; RSM_names[1] = { "Берковец" }; RSM_names[2] = { "Пуд" }; RSM_names[3] = { "Фунт" }; RSM_names[4] = { "Лот" }; RSM_names[5] = { "Золотник" }; RSM_names[6] = { "Выход" };
-		BrSM_names[0] = { "Выберите единицу измерений:" }; BrSM_names[1] = { "Фунт" }; BrSM_names[2] = { "Стоун" }; BrSM_names[3] = { "Короткий квартер" }; BrSM_names[4] = { "Тод (Длинный квартер)" }; BrSM_names[5] = { "Малый хандредвейт (Центал или центнер)" }; BrSM_names[6] = { "Большой хандредвейт (Квинтал)" }; BrSM_names[7] = { "Челдрон (мера веса угля)" }; BrSM_names[8] = { "Киль (мера веса угля)" }; BrSM_names[9] = { "Тонна метрическая" }; BrSM_names[10] = { "Тонна малая" }; BrSM_names[11] = { "Тонна большая" }; BrSM_names[12] = { "Выход" };
-		PhSM_country[0] = { "Выберите, аптекарскую систему мер какой страны Вы хотите использовать:" }; PhSM_country[1] = { "Россия" }; PhSM_country[2] = { "Германия" }; PhSM_country[3] = { "Великобритания" }; PhSM_country[4] = { "Выход" };
-		PhSM_names[0] = { "Выберите единицу измерений:" }; PhSM_names[1] = { "Фунт" }; PhSM_names[2] = { "Унция" }; PhSM_names[3] = { "Драхма" }; PhSM_names[4] = { "Скрупул" }; PhSM_names[5] = { "Гран" }; PhSM_names[6] = { "Выход" };
 	}
-private:
-	void conversation_to_Russian_system_of_measures_RSM()
+
+	double conversation_to_Russian_system_of_measures_RSM(int num)
 	{
-		int num = choise_in_menu(RSM_names, RSM_PhSM_count);
-		if (num != 6)
-		{
-			std::cout << "Единица измерения: " << RSM_names[num] << std::endl;
-			std::cout << "Значение: ";
-		}
 		switch (num)
 		{
 		case 1:
-			conversed_meaning = meaning / 164;
-			break;
+			return(meaning / 164);
 		case 2:
-			conversed_meaning = meaning / 16.4;
-			break;
+			return(meaning / 16.4);
 		case 3:
-			conversed_meaning = meaning / 0.4095120375;
-			break;
+			return(meaning / 0.4095120375);
 		case 4:
-			conversed_meaning = meaning / 0.4095120375 * 32;
-			break;
+			return(meaning / 0.4095120375 * 32);
 		case 5:
-			conversed_meaning = meaning / 0.4095120375 * 96;
-			break;
+			return(meaning / 0.4095120375 * 96);
 		case 6:
-			conversed_meaning = -1;
-			return;
+			return (- 1);
 		}
 	}
 
-	void conversation_to_British_system_of_measures_BrSM()
+	double conversation_to_British_system_of_measures_BrSM(int num)
 	{
-		int num = choise_in_menu(BrSM_names, BrSM_count);
-		if (num != 12)
-		{
-			std::cout << "Единица измерения: " << BrSM_names[num] << std::endl;
-			std::cout << "Значение: ";
-		}
 		switch (num)
 		{
 		case 1:
-			conversed_meaning = meaning / 0.45359237;
-			break;
+			return(meaning / 0.45359237);
 		case 2:
-			conversed_meaning = meaning / 0.45359237 / 14;
-			break;
+			return(meaning / 0.45359237 / 14);
 		case 3:
-			conversed_meaning = meaning / 0.45359237 / 25;
-			break;
+			return(meaning / 0.45359237 / 25);
 		case 4:
-			conversed_meaning = meaning / 0.45359237 / 28;
-			break;
+			return(meaning / 0.45359237 / 28);
 		case 5:
-			conversed_meaning = meaning / 0.45359237 / 100;
-			break;
+			return(meaning / 0.45359237 / 100);
 		case 6:
-			conversed_meaning = meaning / 0.45359237 / 112;
-			break;
+			return(meaning / 0.45359237 / 112);
 		case 7:
-			conversed_meaning = meaning / 0.45359237 / 5936;
-			break;
+			return(meaning / 0.45359237 / 5936);
 		case 8:
-			conversed_meaning = meaning / 0.45359237 / 47488;
-			break;
+			return(meaning / 0.45359237 / 47488);
 		case 9:
-			conversed_meaning = meaning / 0.45359237 / 2204.6;
-			break;
+			return(meaning / 0.45359237 / 2204.6);
 		case 10:
-			conversed_meaning = meaning / 0.45359237 / 2000;
-			break;
+			return(meaning / 0.45359237 / 2000);
 		case 11:
-			conversed_meaning = meaning / 0.45359237 / 2240;
-			break;
+			return(meaning / 0.45359237 / 2240);
 		case 12:
-			conversed_meaning = -1;
-			return;
-		}
-	}
-
-	void conversation_to_Pharmaceutical_system_of_measures_PhSM()
-	{
-		int num = choise_in_menu(PhSM_names, RSM_PhSM_count);
-		if (num != 6)
-		{
-			std::cout << "Единица измерения: " << PhSM_names[num] << std::endl;
-			std::cout << "Значение: ";
-		}
-		switch (num)
-		{
-		case 1:
-			break;
-		case 2:
-			conversed_meaning *= 12;
-			break;
-		case 3:
-			conversed_meaning *= 96;
-			break;
-		case 4:
-			conversed_meaning *= 288;
-			break;
-		case 5:
-			conversed_meaning *= 5760;
-			break;
-		case 6:
-			conversed_meaning = -1;
-			return;
-		}
-	}
-
-	void choise_of_country_of_Pharmaceutical_system_of_measures_PhSM()
-	{
-		switch (choise_in_menu(PhSM_country, PhSM_country_count))
-		{
-		case 1:
-			conversed_meaning = meaning / 0.358323;
-			break;
-		case 2:
-			conversed_meaning = meaning / 0.350783;
-			break;
-		case 3:
-			conversed_meaning = meaning / 0.373242;
-			break;
-		case 4:
-			conversed_meaning = -1;
-			return;
-		}
-		conversation_to_Pharmaceutical_system_of_measures_PhSM();
-	}
-
-public:
-
-	double choise_of_system_of_measure()
-	{
-		switch (choise_in_menu(system_of_measures, systems_of_measure_count))
-		{
-		case 1:
-			conversation_to_Russian_system_of_measures_RSM();
-			break;
-		case 2:
-			conversation_to_British_system_of_measures_BrSM();
-			break;
-		case 3:
-			choise_of_country_of_Pharmaceutical_system_of_measures_PhSM();
-			break;
-		case 4:
 			return(-1);
 		}
-		return(conversed_meaning);
 	}
 
-	void insert_weight()
+	double conversation_to_Pharmaceutical_system_of_measures_PhSM(int num, double coefficient)
 	{
-		std::cout << "Введите текущий вес в килограммах: ";
-		std::cin >> meaning;
-		while (meaning < 0)
+		switch (num)
 		{
-			system("cls");
-			std::cout << "Вес введен некорректно!" << std::endl;
-			std::cout << "Пожалуйста, введите текущий вес в килограммах заново: ";
-			std::cin >> meaning;
+		case 1:
+			return(coefficient * meaning);
+		case 2:
+			return(coefficient * meaning * 12);
+		case 3:
+			return(coefficient * meaning * 96);
+		case 4:
+			return(coefficient * meaning * 288);
+		case 5:
+			return(coefficient * meaning * 5760);
+		case 6:
+			return(-1);
 		}
-		system("cls");
+	}
+
+	void insert_weight(double weight)
+	{
+		meaning = weight;
 		return;
 	}
 
@@ -240,13 +137,29 @@ void begining_ending(Weight w)
 {
 	int num = 0;
 	double final_meaning;
+	double Ph_coefficient = 0.0;
 	std::string actions[actions_count] = { "Что Вы хотите сделать с текущим весом:", "Узнать текущий вес в килограммах", "Изменить текущий вес в килограммах", "Перевести в другую единицу измерения", "Выйти" };
+	std::string system_of_measures[systems_of_measure_count] = { "Выберите систему мер веса", "Русская система мер", "Британская система мер", "Аптекарская система мер", "Выход" };
+	std::string RSM_names[RSM_PhSM_count] = { "Выберите единицу измерений:", "Берковец", "Пуд", "Фунт", "Лот", "Золотник", "Выход" };
+	std::string BrSM_names[BrSM_count] = { "Выберите единицу измерений:", "Фунт", "Стоун", "Короткий квартер", "Тод (Длинный квартер)", "Малый хандредвейт (Центал или центнер)", "Большой хандредвейт (Квинтал)", "Челдрон (мера веса угля)", "Киль (мера веса угля)", "Тонна метрическая", "Тонна малая", "Тонна большая", "Выход" };
+	std::string PhSM_country[PhSM_country_count] = { "Выберите, аптекарскую систему мер какой страны Вы хотите использовать:", "Россия", "Германия", "Великобритания", "Выход" };
+	std::string PhSM_names[RSM_PhSM_count] = { "Выберите единицу измерений:", "Фунт", "Унция", "Драхма", "Скрупул", "Гран", "Выход" };
 	std::cout << "Добрый день!" << std::endl;
 	std::cout << "Вас приветствует конвертер весов. Я помогу Вам перевести вес из килограммов в другие единицы измерения, которые будут представлены позже" << std::endl;
 	std::cout << "Чтобы перемещаться по меню, используйте клавиши 'Вверх' и 'Вниз'. Для выбора из списка нажмите 'Enter'" << std::endl << std::endl;
 	system("pause");
 	system("cls");
-	w.insert_weight();
+	std::cout << "Введите текущий вес в килограммах: ";
+	std::cin >> final_meaning;
+	while (final_meaning < 0)
+	{
+		system("cls");
+		std::cout << "Вес введен некорректно!" << std::endl;
+		std::cout << "Пожалуйста, введите текущий вес в килограммах заново: ";
+		std::cin >> final_meaning;
+	}
+	system("cls");
+	w.insert_weight(final_meaning);
 	do
 	{
 		final_meaning = 0;
@@ -260,21 +173,72 @@ void begining_ending(Weight w)
 			system("cls");
 			break;
 		case 2:
-			w.insert_weight();
+			std::cout << "Введите текущий вес в килограммах: ";
+			std::cin >> final_meaning;
+			while (final_meaning < 0)
+			{
+				system("cls");
+				std::cout << "Вес введен некорректно!" << std::endl;
+				std::cout << "Пожалуйста, введите текущий вес в килограммах заново: ";
+				std::cin >> final_meaning;
+			}
+			system("cls");
+			w.insert_weight(final_meaning);
 			break;
 		case 3:
-			final_meaning = w.choise_of_system_of_measure();
+			switch (choise_in_menu(system_of_measures, systems_of_measure_count))
+			{
+			case 1:
+				num = choise_in_menu(RSM_names, RSM_PhSM_count);
+				final_meaning = w.conversation_to_Russian_system_of_measures_RSM(num);
+				if (final_meaning != -1)
+					std::cout << "Единица измерения: " << RSM_names[num] << std::endl;
+				break;
+			case 2:
+				num = choise_in_menu(BrSM_names, BrSM_count);
+				final_meaning = w.conversation_to_British_system_of_measures_BrSM(num);
+				if (final_meaning != -1)
+					std::cout << "Единица измерения: " << BrSM_names[num] << std::endl;
+				break;
+			case 3:
+				switch (choise_in_menu(PhSM_country, PhSM_country_count))
+				{
+				case 1:
+					Ph_coefficient = 0.358323;
+					break;
+				case 2:
+					Ph_coefficient = 0.350783;
+					break;
+				case 3:
+					Ph_coefficient = 0.373242;
+					break;
+				case 4:
+					num = -1;
+					return;
+				}
+				if (num != -1)
+				{
+					num = choise_in_menu(PhSM_names, RSM_PhSM_count);
+					final_meaning = w.conversation_to_Pharmaceutical_system_of_measures_PhSM(num, Ph_coefficient);
+					if (final_meaning != -1)
+						std::cout << "Единица измерения: " << PhSM_names[num] << std::endl;
+				}
+				break;
+			case 4:
+				final_meaning = -1;
+				break;
+			}
 			if (final_meaning == -1)
 				num = -1;
 			else
 			{
-				std::cout << final_meaning << std::endl << std::endl;
+				std::cout << "Значение: " << final_meaning << std::endl << std::endl;
 				system("pause");
 				system("cls");
 			}
 			break;
 		default:
-			num = -1;;
+			num = -1;
 		}
 	} while (num != -1);
 	std::cout << "До свидания! Хорошего Вам дня)" << std::endl << std::endl;
