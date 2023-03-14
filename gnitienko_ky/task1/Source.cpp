6247f649c850cbde59b443e1542204692e6f804c
Gn4ik <gnitienko220304@yandex.ru>
2023-03-14 16:28:01 +0300

    Ввод и вывод времени в main

--- gnitienko_ky/task1/Source.cpp
+++ gnitienko_ky/task1/Source.cpp
@@ -10,9 +10,9 @@
 using namespace std;
 
 class Time {
-	time_t hours, UserHours, buffHours;
-	time_t minutes, UserMinutes, buffMinutes;
-	time_t seconds, UserSeconds, buffSeconds;
+	time_t hours, UserHours;
+	time_t minutes, UserMinutes;
+	time_t seconds, UserSeconds;
 public:
 	Time() {
 		time_t now = time(0);
@@ -32,21 +32,12 @@ public:
 	void Print2() {
 		cout << "Пользовательское время: " << UserHours << ":" << UserMinutes << ":" << UserSeconds << endl;
 	}
-	void Enter() {
-		cout << "Введите время, на которое будет сдвинуто текущее" << endl;
-		cout << "Часы: ";
-		cin >> buffHours;
-		cout << "Минуты: ";
-		cin >> buffMinutes;
-		cout << "Секунды: ";
-		cin >> buffSeconds;
-	}
 	void Difference() {
 		cout << "Разница между текущим и пользовательским временем: " << abs(hours - UserHours) << ":" << abs(minutes - UserMinutes) << ":" << abs(seconds - UserSeconds) << endl;
 	}
-	void PlusTime() {
+	int PlusHour(int h, int m, int s) {
 		int hour, minute, sec;
-		int buffHours1 = buffHours, buffMinutes1 = buffMinutes, buffSeconds1 = buffSeconds;
+		int buffHours1 = h , buffMinutes1 = m, buffSeconds1 = s;
 		if (buffSeconds1 / 60 > 0)  buffMinutes1 += (buffSeconds1 / 60);
 		if ((buffSeconds1 % 60) + seconds > 60) buffMinutes1++;
 		if (buffMinutes1 / 60 > 0)  buffHours1 += (buffMinutes1 / 60);
@@ -54,34 +45,75 @@ public:
 		while (((hours + buffHours1) % 24 <= 24) && ((hours + buffHours1) % 24 >= 0)) { hour = hours + buffHours1; buffHours1 -= 24; }
 		while (((minutes + buffMinutes1) % 60 <= 60) && ((minutes + buffMinutes1) % 60 >= 0)) { minute = minutes + buffMinutes1; buffMinutes1 -= 60; }
 		while (((seconds + buffSeconds1) % 60 <= 60) && ((seconds + buffSeconds1) % 60 >= 0)) { sec = seconds + buffSeconds1; buffSeconds1 -= 60; }
-		cout << "Время со сдвигом в большую сторону: " << hour << ":" << minute << ":" << sec << endl;
+		return hour;
 	}
-	void MinusTime() {
+	int PlusMinute(int h, int m, int s) {
 		int hour, minute, sec;
-		int buffHours2 = buffHours, buffMinutes2 = buffMinutes, buffSeconds2 = buffSeconds;
-		if (buffSeconds2 / 60 > 0) { buffMinutes2 += (buffSeconds2 / 60); buffSeconds2 = buffSeconds % 60; }
+		int buffHours1 = h, buffMinutes1 = m, buffSeconds1 = s;
+		if (buffSeconds1 / 60 > 0)  buffMinutes1 += (buffSeconds1 / 60);
+		if ((buffSeconds1 % 60) + seconds > 60) buffMinutes1++;
+		if (buffMinutes1 / 60 > 0)  buffHours1 += (buffMinutes1 / 60);
+		if ((buffMinutes1 % 60) + minutes > 60) buffHours1++;
+		while (((hours + buffHours1) % 24 <= 24) && ((hours + buffHours1) % 24 >= 0)) { hour = hours + buffHours1; buffHours1 -= 24; }
+		while (((minutes + buffMinutes1) % 60 <= 60) && ((minutes + buffMinutes1) % 60 >= 0)) { minute = minutes + buffMinutes1; buffMinutes1 -= 60; }
+		while (((seconds + buffSeconds1) % 60 <= 60) && ((seconds + buffSeconds1) % 60 >= 0)) { sec = seconds + buffSeconds1; buffSeconds1 -= 60; }
+		return minute;
+	}
+	int PlusSec(int h, int m, int s) {
+		int hour, minute, sec;
+		int buffHours1 = h, buffMinutes1 = m, buffSeconds1 = s;
+		if (buffSeconds1 / 60 > 0)  buffMinutes1 += (buffSeconds1 / 60);
+		if ((buffSeconds1 % 60) + seconds > 60) buffMinutes1++;
+		if (buffMinutes1 / 60 > 0)  buffHours1 += (buffMinutes1 / 60);
+		if ((buffMinutes1 % 60) + minutes > 60) buffHours1++;
+		while (((hours + buffHours1) % 24 <= 24) && ((hours + buffHours1) % 24 >= 0)) { hour = hours + buffHours1; buffHours1 -= 24; }
+		while (((minutes + buffMinutes1) % 60 <= 60) && ((minutes + buffMinutes1) % 60 >= 0)) { minute = minutes + buffMinutes1; buffMinutes1 -= 60; }
+		while (((seconds + buffSeconds1) % 60 <= 60) && ((seconds + buffSeconds1) % 60 >= 0)) { sec = seconds + buffSeconds1; buffSeconds1 -= 60; }
+		return sec;
+	}
+	int MinusHour(int h, int m, int s) {
+		int hour;
+		int buffHours2 = h, buffMinutes2 = m, buffSeconds2 = s;
+		if (buffSeconds2 / 60 > 0) { buffMinutes2 += (buffSeconds2 / 60); buffSeconds2 = s % 60; }
 		if (buffMinutes2 / 60 > 0) { buffHours2 += (buffMinutes2 / 60); buffMinutes2 = buffMinutes2 % 60; }
 		buffHours2 = buffHours2 % 24;
 		if (hours - buffHours2 < 0) {
 			hour = (hours + 24) - buffHours2;
 		}
 		else hour = hours - buffHours2;
+		return hour;
+	}
+	int MinusMinute(int h, int m, int s) {
+		int minute;
+		int buffHours2 = h, buffMinutes2 = m, buffSeconds2 = s;
+		if (buffSeconds2 / 60 > 0) { buffMinutes2 += (buffSeconds2 / 60); buffSeconds2 = s % 60; }
+		if (buffMinutes2 / 60 > 0) { buffHours2 += (buffMinutes2 / 60); buffMinutes2 = buffMinutes2 % 60; }
+		buffHours2 = buffHours2 % 24;
 		//******************************************//
 		if (minutes - buffMinutes2 < 0) {
 			minute = (minutes + 60) - buffMinutes2;
 		}
 		else minute = minutes - buffMinutes2;
-		//******************************************//
+		return minute;
+	}
+	int MinusSec(int h, int m, int s) {
+		int sec;
+		int buffHours2 = h, buffMinutes2 = m, buffSeconds2 = s;
+		if (buffSeconds2 / 60 > 0) { buffMinutes2 += (buffSeconds2 / 60); buffSeconds2 = s % 60; }
+		if (buffMinutes2 / 60 > 0) { buffHours2 += (buffMinutes2 / 60); buffMinutes2 = buffMinutes2 % 60; }
+		buffHours2 = buffHours2 % 24;
+
 		if (seconds - buffSeconds2 < 0) {
 			sec = (seconds + 60) - buffSeconds2;
 		}
 		else sec = seconds - buffSeconds2;
-		cout << "Время со сдвигом в меньшую сторону: " << hour << ":" << minute << ":" << sec << endl;
+		return sec;
 	}
 };
 
 void main() {
 	int a, b, c;
+	int h, m, s;
 	setlocale(LC_CTYPE, "Rus");
 	Time t;
 	t.Print1();
@@ -111,9 +143,15 @@ void main() {
 	system("pause");
 	system("cls");
 	//*********************************************//
-	t.Print1();
-	t.Enter();
-	t.PlusTime();
-	t.MinusTime();
+	t.Print1(); 
+	cout << "Введите время, на которое будет сдвинуто текущее" << endl;
+	cout << "Часы: ";
+	cin >> h;
+	cout << "Минуты: ";
+	cin >> m;
+	cout << "Секунды: ";
+	cin >> s;
+	cout << "Время со сдвигом в большую сторону: " << t.PlusHour(h, m, s) << ":" << t.PlusMinute(h, m, s) << ":" << t.PlusSec(h, m, s) << endl;
+	cout << "Время со сдвигом в меньшую сторону: " << t.MinusHour(h, m, s) << ":" << t.MinusMinute(h, m, s) << ":" << t.MinusSec(h, m, s) << endl;
 	system("pause");
 }
