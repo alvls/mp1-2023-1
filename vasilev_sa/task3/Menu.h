49039dcfaddfdfb2708ce269cc03936b720c38f1
venn2713 <venn2713@gmail.com>
2023-03-31 16:51:55 +0300

    Global Remake

---
+++ vasilev_sa/task3/Menu.h
@@ -0,0 +1,129 @@
+#pragma once
+#include <string>
+#include "console.h"
+#include <conio.h>
+#include <functional>
+
+class Menu {
+private:
+  int size;
+  std::string* menu;
+  int point;
+
+public:
+  Menu(int Size, int Item = 0) {
+    size = Size;
+    point = Item;
+    menu = new std::string[size];
+    for (int i = 0; i < size; i++)
+      menu[i] = "";
+  }
+
+  Menu(const Menu& Another) {
+    size = Another.size;
+    point = Another.point;
+    menu = new std::string[size];
+    for (int i = 0; i < size; i++)
+      menu[i] = Another.menu[i];
+  }
+
+  ~Menu() {
+    delete[] menu;
+  }
+
+  int Get_size() {
+    return size;
+  }
+
+  void Set_name(int Point, std::string Name) {
+    if (Point < 0 || Point >= size)
+      return;
+    menu[Point] = Name;
+  }
+
+  void AddItem(std::string Name) {
+    std::string* new_menu = new std::string[size + 1];
+    if (new_menu == nullptr) {
+      std::cout << "Error allocating memory" << std::endl;
+      return;
+    }
+    for (int i = 0; i < size; i++)
+      new_menu[i] = menu[i];
+    new_menu[size] = Name;
+    size++;
+    delete[] menu;
+    menu = new_menu;
+  }
+
+  void RemoveItem(int Point) {
+    if (Point < 0 || Point >= size)
+      return;
+    std::string* new_menu = new std::string[size - 1];
+    if (new_menu == nullptr) {
+      std::cout << "Error allocating memory" << std::endl;
+      return;
+    }
+    for (int i = 0, j = 0; i < size; i++) {
+      if (i == Point)
+        continue;
+      new_menu[j] = menu[i];
+      j++;
+    }
+    size--;
+    delete[] menu;
+    menu = new_menu;
+    if (point >= size)
+      point = size - 1;
+  }
+
+  void Print(int x = 0, int y = 0, std::function<void(int)> callback = nullptr) {
+    char ch;
+    bool flag = true;
+    hidecursor();
+    while (flag) {
+      for (int i = 0; i < size; i++) {
+        if (i == point)
+          textcolor(YELLOW);
+        else
+          textcolor(LIGHTGRAY);
+        gotoxy(x, y + i + 1);
+        std::cout << i + 1 << ". " << menu[i] << std::endl;
+      }
+      ch = _getch();
+      if (ch == ARROW)
+        ch = _getch();
+      switch (ch) {
+      case UP:
+        if (point > 0)
+          point--;
+        break;
+      case DOWN:
+        if (point < size - 1)
+          point++;
+        break;
+      case ESCAPE:
+        exit(0);
+        break;
+      case ENTER:
+      case SPACE:
+      case RIGHT:
+        flag = false;
+        system("cls");
+        gotoxy(x, y);
+        if (callback != nullptr)
+          callback(point + 1);
+        gotoxy(x, y + 1);
+        system("PAUSE");
+        break;
+      }
+      system("cls");
+    }
+  }
+};
+
+void myCallback(int selectedItem)
+{
+  // Действия, которые нужно выполнить при выборе пользователем определенного пункта меню
+  textcolor(LIGHTGRAY);
+  std::cout << "You chose menu item " << selectedItem << std::endl;
+}
--- vasilev_sa/task3/console.h
+++ vasilev_sa/task3/console.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <windows.h>
 #include <stdio.h>
 #include <memory.h>
@@ -31,48 +32,80 @@ void gotoxy(int x, int y)
   if (!Console)
     return;
 
-  COORD pos;
+  CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+  GetConsoleScreenBufferInfo(Console, &bufferInfo);
+  SHORT width = bufferInfo.dwSize.X;
+  SHORT height = bufferInfo.dwSize.Y;
+
+  // Проверка, что координаты находятся в пределах размеров окна
+  if (x < 0 || x >= width || y < 0 || y >= height)
+  {
+    // Установка размеров консольного окна, если координаты выходят за его пределы
+    width = max(x + 1, width);
+    height = max(y + 1, height);
+    COORD size = { width, height };
+    SetConsoleScreenBufferSize(Console, size);
+  }
+
+  COORD pos = { 0, 0 };
   pos.X = x;
   pos.Y = y;
 
   SetConsoleCursorPosition(Console, pos);
 }
 
-void hidecursor(void)
+void hidecursor()
 {
-    HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (!Console)
-        return;
-
-    CONSOLE_CURSOR_INFO buf;
-    GetConsoleCursorInfo(Console, &buf);
-    buf.bVisible = 0;
-    SetConsoleCursorInfo(Console, &buf);
+  CONSOLE_CURSOR_INFO cursor_info = { 1, false };
+  SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
 }
 
 void textcolor(int color)
 {
-  HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
-  if (!Console)
+  // Получаем дескриптор консоли
+  HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+  if (console == INVALID_HANDLE_VALUE) {
+    // В случае ошибки выводим сообщение и завершаем функцию
+    perror("Cannot retrieve console handle");
     return;
+  }
 
-  CONSOLE_SCREEN_BUFFER_INFO buf;
-  GetConsoleScreenBufferInfo(Console, &buf);
+  // Получаем информацию о буфере экрана
+  CONSOLE_SCREEN_BUFFER_INFO buffer_info;
+  if (!GetConsoleScreenBufferInfo(console, &buffer_info)) {
+    // В случае ошибки выводим сообщение и завершаем функцию
+    perror("Cannot retrieve console buffer info");
+    return;
+  }
 
-  WORD Attr;
-  DWORD Count;
-  COORD pos = buf.dwCursorPosition;
-  ReadConsoleOutputAttribute(Console, &Attr, 1, pos, &Count);
+  // Получаем текущий цвет текста и фона
+  WORD current_attributes = buffer_info.wAttributes;
 
-  int bk_col = Attr / 16;
-  int col = color % 16;
-  col = col + bk_col * 16;
+  // Получаем код цвета текста, который нужно установить
+  int new_text_color = color % 16;
+  if (new_text_color < 0) {
+    new_text_color += 16;
+  }
 
-  SetConsoleTextAttribute(Console, col);
+  // Получаем код цвета фона
+  int background_color = current_attributes / 16;
 
-  char ch;
-  pos = wherexy();
-  ReadConsoleOutputCharacter(Console, &ch, 1, pos, &Count);
-  printf("%c", ch);
-  gotoxy(pos.X, pos.Y);
+  // Обновляем цвет текста и фона
+  WORD new_attributes = (background_color << 4) | new_text_color;
+  if (!SetConsoleTextAttribute(console, new_attributes)) {
+    // В случае ошибки выводим сообщение и завершаем функцию
+    perror("Cannot set console text attribute");
+    return;
+  }
+
+  // Сохраняем позицию курсора
+  COORD cursor_pos = buffer_info.dwCursorPosition;
+
+  // Восстанавливаем цвет текста на экране в позиции курсора
+  DWORD chars_written;
+  if (!WriteConsoleOutputAttribute(console, &new_attributes, 1, cursor_pos, &chars_written)) {
+    // В случае ошибки выводим сообщение и завершаем функцию
+    perror("Cannot write console output attribute");
+    return;
+  }
 }
