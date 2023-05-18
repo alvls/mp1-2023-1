e6aaa2b4bda542238d9eeb7efa315d885404614f
Кирилл Хасаньянов <hasanyanovk@gmail.com>
2023-05-18 23:11:49 +0300

    Task 6

--- khasanyanov_kv/task6/Source.cpp
+++ khasanyanov_kv/task6/Source.cpp
@@ -8,7 +8,7 @@
 #include <windows.h>
 #include "Header.h"
 using namespace std;
-enum Direction {LEFT, RIGHT, UP, DOWN };
+enum Direction { LEFT, RIGHT, UP, DOWN };
 
 
 
@@ -20,7 +20,7 @@ class Field
     char border;
 public:
     Field() : Field(40, 20, '#') {}
-    Field(int w,  int h, char ch) : width(w), height(h), border(ch) {}
+    Field(int w, int h, char ch) : width(w), height(h), border(ch) {}
     int getWidth() const { return width; }
     int getHeight() const { return height; }
     char getBorder() const { return border; }
@@ -34,12 +34,12 @@ class Snake
     char tail;
     int x;
     int y;
-    int TailX[100] = { 0 };
-    int TailY[100] = { 0 };
+    int TailX[100];
+    int TailY[100];
     int length;
 public:
     Snake() : Snake(0, LEFT, 'O', 'o', 0, 0, Field()) {}
-    Snake(int l, Direction dir, char hch, char tch, int X, int Y, Field field) : length(l), direction(dir), head(hch), tail(tch), x(X), y(Y), f(field){ }
+    Snake(int l, Direction dir, char hch, char tch, int X, int Y, Field field) : length(l), direction(dir), head(hch), tail(tch), x(X), y(Y), f(field) { }
 
     Direction getDirection() const { return direction; }
     char getHead() const { return head; }
@@ -64,7 +64,7 @@ class Fruit
     int y;
 public:
     Fruit() : Fruit(1, 1, '@', Field()) {}
-    Fruit(int X, int Y, char fch, Field field) : f(field), fruit(fch) , x(X), y(Y){}
+    Fruit(int X, int Y, char fch, Field field) : f(field), fruit(fch), x(X), y(Y) {}
     char getFruit() { return fruit; }
     int getX() const { return x; }
     int getY() const { return y; }
@@ -82,7 +82,7 @@ class SnakeGame
         LOSE,
         GAME
     };
-    int score = 0;
+    int score;
     int record;
     int finish = INT_MAX;
     Result res = GAME;
@@ -92,14 +92,14 @@ class SnakeGame
     Fruit fruit;
     Direction dir = snake.getDirection();
 public:
-    SnakeGame() : snake(Snake()), field(Field()), fruit(Fruit()), mode(HARD) {}
-    SnakeGame(Field f, Snake s, Fruit fr, Mode m) : mode(m), field(f), snake(s), fruit(fr) {  }
+    SnakeGame() : snake(Snake()), field(Field()), fruit(Fruit()), mode(HARD) { score = snake.getLength() * 10; }
+    SnakeGame(Field f, Snake s, Fruit fr, Mode m) : mode(m), field(f), snake(s), fruit(fr) { score = s.getLength() * 10; }
 
     void setup()
     {
         srand(time(NULL));
         ifstream f;
-        f.open("SnakeRecord");
+        f.open("SnakeRecord.txt");
         if (!f.is_open())
             throw exception("error");
         f >> record;
@@ -168,12 +168,12 @@ public:
         for (int i = 0; i < field.getWidth() + 2; i++)
             cout << field.getBorder();
         cout << endl;
-        cout << "Ñ÷¸ò: " << score << " Ðåêîðä: "<< record << endl;
+        cout << "Ñ÷¸ò: " << score << " Ðåêîðä: " << record << endl;
     }
 
     void Input()
     {
-        
+
         if (_kbhit())
         {
 
@@ -218,7 +218,7 @@ public:
         int prevPosX = snake.getTailX(0);
         int prevPosY = snake.getTailY(0);
         int prev2PosX, prev2PosY;
-        snake.setTailX(0,snake.getX());
+        snake.setTailX(0, snake.getX());
         snake.setTailY(0, snake.getY());
         for (int i = 1; i < snake.getLength(); i++)
         {
@@ -256,9 +256,9 @@ public:
             else if (snake.getX() < 0)
                 snake.setX(field.getWidth() - 1);
             if (snake.getY() >= field.getHeight())
-                snake.setX(0);
+                snake.setY(0);
             else if (snake.getY() < 0)
-                snake.setX(field.getHeight() - 1);
+                snake.setY(field.getHeight() - 1);
             for (int i = 0; i < snake.getLength(); i++)
                 if (snake.getTailX(i) == snake.getX() && snake.getTailY(i) == snake.getY())
                 {
@@ -304,19 +304,23 @@ public:
         if (score > record)
         {
             ofstream f;
-            f.open("SnakeRecord");
+            f.open("SnakeRecord.txt");
             f << score;
             f.close();
             ifstream fi;
-            fi.open("SnakeRecord");
+            fi.open("SnakeRecord.txt");
             fi >> record;
             fi.close();
         }
-            
+
     }
 
     void Start(int fin)
     {
+        if (finish <= score)
+        {
+            exit(0);
+        }
         finish = fin;
         setup();
         while (res == GAME)
@@ -327,7 +331,7 @@ public:
             Sleep(30);
         }
     }
-    void Start() { Start(INT_MAX);}
+    void Start() { Start(INT_MAX); }
 
 
     void End()
