f1f71cabe191632437828271081ca04f3a33c23d
odincovm <odincovmisa16@gmail.com>
2023-05-22 10:33:52 +0300

    task6

---
+++ Odinsov_MV/task6/source.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <locale.h>
+#include <math.h>
+#include <string>
+using namespace std;
+class Randomnumber {
+protected:
+	int len;
+	int number = 1;
+public:
+	Randomnumber(int n) :len(n) {
+		srand(time(NULL));
+		vector <int> figures = { 1,2,3,4,5,6,7,8,9 };
+		int tmp1 = rand() % figures.size();
+		int tmp2 = len - 1;
+		int tmp3;
+		number *= figures[tmp1] * pow(10, tmp2);
+		tmp2--;
+		figures.erase(figures.begin() + tmp1);
+		figures.push_back(0);
+		for (int i = 0; i < len - 1; i++) {
+			int tmp1 = rand() % figures.size();
+			tmp3 = pow(10, tmp2);
+			number += figures[tmp1] * tmp3;
+			tmp2--;
+			figures.erase(figures.begin() + tmp1);
+		}
+		cout << number << endl;
+	}
+
+	virtual ~Randomnumber() = 0 {}
+};
+class bullandcow :Randomnumber {
+
+public:
+	bullandcow(int n) :Randomnumber(n) { }
+	int bull(int ans) {
+		int Bull = 0;
+		string tmp1 = to_string(number);
+		string tmp2 = to_string(ans);
+		for (int i = 0; i < tmp1.size(); i++) {
+			if (tmp1[i] == tmp2[i])
+				Bull++;
+		}
+		return Bull;
+	}
+	int cow(int ans) {
+		int Cow = 0;
+		string tmp1 = to_string(number);
+		string tmp2 = to_string(ans);
+		for (int i = 0; i < tmp1.size(); i++) {
+			for (int j = 0; j < tmp2.size(); j++) {
+				if ((i != j) && (tmp1[i] == tmp2[j]))
+					Cow++;
+			}
+		}
+		return Cow;
+	}
+	bool is_win(int ans) {
+		if (bull(ans) == to_string(ans).size())
+			return true;
+		return false;
+	}
+};
+void main() {
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите длину номера\n";
+	int n;
+	cin >> n;
+	bullandcow M(n);
+	int ans = -1;
+	while (1)
+	{
+		cout << "Введите предположение\n";
+		cin >> ans;
+		cout << "Количество быков " << M.bull(ans) << endl;
+		cout << "Количество коров " << M.cow(ans) << endl;
+		if (M.is_win(ans)) {
+			cout << "Ты победил";
+			break;
+		}
+
+	}
+
+
+	system("pause");
+}
