c5ee6837a8e34136fdef10d06b745c7181906074
Sergey Vasilev <112872559+venn2713@users.noreply.github.com>
2023-06-08 18:13:42 +0300

    Update Header.h

--- vasilev_sa/task5/Header.h
+++ vasilev_sa/task5/Header.h
@@ -6,7 +6,7 @@ bool is_valid_date(int day, int mon)
   if (day < 1 || day > 31 || mon < 1 || mon > 12)
     return false;
   std::time_t now = std::time(nullptr);
-  std::tm *local_time = std::localtime(&now);
+  std::tm* local_time = std::localtime(&now);
   int curr_mon = local_time->tm_mon + 1;
   int curr_day = local_time->tm_mday;
   int mon_diff = mon - curr_mon;
@@ -30,51 +30,45 @@ enum Train_type
   firmtrain,
 };
 
-struct Railway_data
-{
-  int tm_day; // дни от 1 до 31
-  int tm_mon; // месяцы от 1 до 11
-  Railway_data() : tm_day(0), tm_mon(0) {}
-  Railway_data(int day, int mon) : tm_day(day), tm_mon(mon) {}
+struct Railway_data {
+  int tm_day;  // дни от 1 до 31
+  int tm_mon;   // месяцы от 1 до 11
+  Railway_data(): tm_day(0), tm_mon(0) {}
+  Railway_data(int day, int mon): tm_day(day), tm_mon(mon) {}
 
   // Перегрузка префиксного инкремента
-  Railway_data &operator++()
-  {
-    if (tm_day == 31)
-    {
+  Railway_data& operator++() {
+    if (tm_day == 31) {
       tm_day = 1;
-      if (tm_mon == 11)
-      {
+      if (tm_mon == 11) {
         tm_mon = 0;
       }
-      else
-      {
+      else {
         ++tm_mon;
       }
     }
-    else
-    {
+    else {
       ++tm_day;
     }
     return *this;
   }
 
   // Перегрузка постфиксного инкремента
-  Railway_data operator++(int)
-  {
+  Railway_data operator++(int) {
     Railway_data tmp(*this);
     ++(*this);
     return tmp;
   }
 
-  Railway_data &operator=(const Railway_data data)
+
+  Railway_data& operator=(const Railway_data data)
   {
     tm_day = data.tm_day;
     tm_mon = data.tm_mon;
     return *this;
   }
 
-  bool operator==(const Railway_data &data)
+  bool operator==(const Railway_data& data)
   {
     return tm_day == data.tm_day && tm_mon == data.tm_mon;
   }
@@ -82,14 +76,13 @@ struct Railway_data
 
 struct FIO
 {
-
   std::string Last_name;
   std::string First_name;
   std::string Patronymic;
-  FIO() : First_name(""), Last_name(""), Patronymic("") {}
-  FIO(std::string f, std::string i, std::string o) : First_name(i), Last_name(f), Patronymic(o) {}
+  FIO() :First_name(""), Last_name(""), Patronymic("") {}
+  FIO(std::string f, std::string i, std::string o): First_name(i), Last_name(f), Patronymic(o) {}
 
-  FIO &operator=(const FIO fio)
+  FIO& operator= (const FIO fio)
   {
     Last_name = fio.Last_name;
     First_name = fio.First_name;
@@ -105,14 +98,13 @@ struct FIO
   }
 };
 
-std::ostream &operator<<(std::ostream &os, const Railway_data &data)
-{
+std::ostream& operator<<(std::ostream& os, const Railway_data& data) {
   os << std::setfill('0') << std::setw(2) << data.tm_day << "."
-     << std::setfill('0') << std::setw(2) << data.tm_mon;
+    << std::setfill('0') << std::setw(2) << data.tm_mon;
   return os;
 }
 
-std::ostream &operator<<(std::ostream &os, const FIO &fio)
+std::ostream& operator<<(std::ostream& os, const FIO& fio)
 {
   os << fio.Last_name << " " << fio.First_name << " " << fio.Patronymic;
   return os;
